60f32162af95d43f268b2d98e3c97a1678210239
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-07-03 17:11:29 +0530

    Basic Array Initialization Operation

---
+++ Basic_ArrayInitialiseandPrint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //To initialise an int array
+   int arr[5]={10,20,30,40,50};
+   
+   //To print the array elements we write For loop
+   for(int i=0;i<5;i++)
+   {
+       cout<<arr[i]<<endl;
+   }
+   return 0;
+}
