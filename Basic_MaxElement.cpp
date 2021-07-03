a9a3b45c4c3e6b7a554e6b3b3822872ee97b1dd6
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-07-03 18:26:50 +0530

    Basic Max Elements in an array when the elements are already given in an array

---
+++ Basic_MaxElement.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//when the elements are already given in an array
+
+int main()
+{
+    int arr[5]={500,400,200,7000,100};
+    for(int i=0;i<5;i++)
+    {
+        cout<<"The arry elements are "<<arr[i]<<endl;
+        
+        
+    }
+    for(int i=0;i<5;i++)
+    {
+        if(arr[0]<arr[i])
+        {
+            arr[0]=arr[i];
+            cout<<"the largest element is "<<arr[0]<<endl;
+        }
+    }
+    
+}
